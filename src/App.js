193caae166dfca467648ff3ef6fb8b5451b737a0
Ashley Yan <Asyan77@gmail.com>
2023-08-04 17:29:19 -0700

    edit useEffect in App.js for feather colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Message from "./components/Message";
 import PictureDisplay from "./components/PictureDisplay";
 
 function App() {
-  const [size, setSize] = useState('s');
+  const [size, setSize] = useState("s");
   const [featherCount, setFeatherCount] = useState(0);
   const [featherColors, setFeatherColors] = useState([]);
   const [isRed, setIsRed] = useState(false);
@@ -11,6 +11,35 @@ function App() {
   const [isBrown, setIsBrown] = useState(false);
   const [isLightBrown, setIsLightBrown] = useState(false);
   const [isYellow, setIsYellow] = useState(false);
+
+  useEffect(() => {
+    const colors = [];
+    let isColor = isRed;
+
+    if (isRed) {
+      console.log("what is red???", isRed);
+      colors.push("red");
+    }
+    if (isOrange) {
+      colors.push("orange");
+    }
+    if (isLightBrown) {
+      colors.push("light-brown");
+    }
+
+    if (isYellow) {
+      colors.push("yellow");
+    }
+    if (isBrown) {
+      colors.push("brown");
+    }
+    setFeatherColors(colors);
+    // console.log('red', isRed);
+    // console.log('orange', isOrange);
+    // console.log('brown', isBrown);
+    // console.log('light brown', isLightBrown);
+    // console.log('yellow', isYellow);
+  }, [isBrown, isRed, isLightBrown, isOrange, isYellow]);
 
   return (
     <>
@@ -20,10 +49,10 @@ function App() {
       {/* User controls */}
       <div className="button-controls">
         Size:
-        <button onClick={() => setSize('s')}>Small</button>
-        <button onClick={() => setSize('m')}>Medium</button>
-        <button onClick={() => setSize('l')}>Large</button>
-        <button onClick={() => setSize('xl')}>X-Large</button>
+        <button onClick={() => setSize("s")}>Small</button>
+        <button onClick={() => setSize("m")}>Medium</button>
+        <button onClick={() => setSize("l")}>Large</button>
+        <button onClick={() => setSize("xl")}>X-Large</button>
       </div>
       <div className="button-controls">
         Feather Count:
@@ -37,26 +66,41 @@ function App() {
       </div>
       <div className="button-controls">
         Feather Color(s):
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsRed(e.currentTarget.checked)}
-        />Red</label>
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsOrange(e.currentTarget.checked)}
-        />Orange</label>
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsBrown(e.currentTarget.checked)}
-        />Brown</label>
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsLightBrown(e.currentTarget.checked)}
-        />Light Brown</label>
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsYellow(e.currentTarget.checked)}
-        />Golden Yellow</label>
+        <label>
+          <input
+            type="checkbox"
+            onChange={(e) => setIsRed(e.currentTarget.checked)}
+          />
+          Red
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            onChange={(e) => setIsOrange(e.currentTarget.checked)}
+          />
+          Orange
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            onChange={(e) => setIsBrown(e.currentTarget.checked)}
+          />
+          Brown
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            onChange={(e) => setIsLightBrown(e.currentTarget.checked)}
+          />
+          Light Brown
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            onChange={(e) => setIsYellow(e.currentTarget.checked)}
+          />
+          Golden Yellow
+        </label>
       </div>
 
       {/* Generated display based on user selections above */}
